c72c0d713ab7d584a7dd016ed9619721ea583f7a
Hernando17 <ginkotsu17@gmail.com>
2022-01-29 19:26:23 +0700

    Latest Update

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Portfolio | Hernando</title>
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css" class="" />
+  <link rel="stylesheet" href="assets/css/style.css" class="" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@300;400;700&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+</head>
+
+<body>
+
+
+  <!-- Jumbotron -->
+  <section id="hero">
+    <div class="hero">
+      <!-- Navbar -->
+      <nav>
+        <ul>
+          <h3>Hernando</h3>
+        </ul>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+        <div class="menu-toggle">
+          <input type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>
+
+      <!-- Hero -->
+      <img src="assets/img/AKINATOR HERNANDO.jpg" width="170px" alt="" class="rounded-circle mt-5" />
+      <h3 class="mt-3">Hernando</h3>
+      <p>Male</p>
+    </div>
+  </section>
+
+  <!-- Jumbotron End -->
+
+  <!-- Content -->
+  <main>
+    <div class="container" style="margin-top:100px;">
+      <h1 class="text-center">Introduction</h1><br>
+      <p class="text-center">
+        Hello.. my name is Hernando, people usually call me as "Nando", "Her", or even "Hernando"<br>
+        My birthday is August 25th, 2004. I was born in Indonesia, yes i'm indonesian.<br>
+        So i can speak bahasa and little english.
+      </p>
+    </div>
+    <div class="container" style="margin-top:200px;">
+      <h1 class="text-center">Interest</h1><br>
+    </div>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="assets/img/gd.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Graphic Design / Video Editing</h5>
+            <p>Digital Art</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="assets/img/programming.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Programming</h5>
+            <p>Web Programming</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="assets/img/photography.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Photography</h5>
+            <p>Image Compotition</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="assets/img/automotive.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Automotive</h5>
+            <p>Car & Racing</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="assets/img/gadget.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Gadget</h5>
+            <p>Latest Technology</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    <div style="padding:100vh"></div>
+  </main>
+  <!-- Content End -->
+  <footer>
+    <div class="foot">
+      <li><a class="bi bi-instagram" href="#"></a></li>
+      <li><a class="bi bi-facebook" href="#"></a></li>
+      <li><a class="bi bi-youtube" href="#"></a></li>
+    </div>
+  </footer>
+
+  <!-- Javascript -->
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/script.js"></script>
+</body>
+
+</html>
