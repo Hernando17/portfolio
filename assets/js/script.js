81013cfaaefc127a56eaf0d658448d67709230ea
Hernando17 <ginkotsu17@gmail.com>
2022-06-12 13:12:57 +0700

    new update

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+ $(document).ready(function () {
+      $("#hamburger").click(function () {
+        $("#dropdown").toggleClass("show");
+        $("#bar1").toggleClass("show");
+        $("#bar2").toggleClass("show");
+        $("#bar3").toggleClass("show");
+      });
+ });
+    
