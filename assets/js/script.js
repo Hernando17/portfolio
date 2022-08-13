1a512f7e115bbcd9a8cb0a17447bc8a408898dd9
Hernando17 <ginkotsu17@gmail.com>
2022-08-13 23:15:06 +0700

    Particle Lib

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
- $(document).ready(function () {
-      $("#hamburger").click(function () {
-        $("#dropdown").toggleClass("show");
-        $("#bar1").toggleClass("show");
-        $("#bar2").toggleClass("show");
-        $("#bar3").toggleClass("show");
-      });
- });
-    
+$(document).ready(function () {
+  $("#hamburger").click(function () {
+    $("#dropdown").toggleClass("show");
+    $("#bar1").toggleClass("show");
+    $("#bar2").toggleClass("show");
+    $("#bar3").toggleClass("show");
+  });
+});
